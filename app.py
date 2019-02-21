f3b065816e511dd32712ef4e4d13a65aa6d662ff
unknown <tengyuh@gmail.com>
2019-02-21 16:15:25 +0800

    save project

---
+++ app.py
@@ -0,0 +1,92 @@
+from flask import Flask
+from datetime import datetime
+import re
+from flask import render_template
+import os
+from flask import request, jsonify
+
+
+app = Flask(__name__)
+
+books = [
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1992'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'published': '1975'}
+]
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/test")
+def test():
+    x = os.system("C:\\a.bat")
+    return "批次執行完成!"
+
+@app.route('/api', methods=['GET'])
+def api():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+@app.route('/api/v1/resources/books/all', methods=['GET'])
+def api_all():
+    return jsonify(books)
+
+
+@app.route('/api/v1/resources/books', methods=['GET'])
+def api_id():
+    # Check if an ID was provided as part of the URL.
+    # If ID is provided, assign it to a variable.
+    # If no ID is provided, display an error in the browser.
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    # Create an empty list for our results
+    results = []
+
+    # Loop through the data and match results that fit the requested ID.
+    # IDs are unique, but other fields might return many results
+    for book in books:
+        if book['id'] == id:
+            results.append(book)
+
+    # Use the jsonify function from Flask to convert our list of
+    # Python dictionaries to the JSON format.
+    return jsonify(results)
+
+
+
+@app.route("/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    # BAD CODE! Avoid inline HTML for security reason, plus templates automatically escape HTML content.
+    content = "<strong>Hello there, " + name + "!</strong> It's " + formatted_now +'***'
+
+    return render_template(
+        "hello_there.html", name = name,
+        title="Hello, Flask",
+        content=content
+    )
+
+if __name__ == "__main__":
+    app.run()
